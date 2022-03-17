2a671a0a3ee7a2d99de39c8d4adda7592347682b
André de Sousa Lacerda <a.de.sousa.lacerda@avanade.com>
2022-03-17 13:42:24 -0300

    update do projeto

---
+++ 06-cli/test.js
@@ -0,0 +1,24 @@
+const { deepStrictEqual, ok } = require("assert");
+const database = require("./database");
+
+const DEFAULT_ITEM_CADASTRAR = {
+  nome: "Flash",
+  poder: "Speed",
+  id: 1,
+};
+
+describe("Suite de manipular de Herois", () => {
+  it("deve pesquisar um heroi usando arquivos", async () => {
+    const expected = DEFAULT_ITEM_CADASTRAR;
+    const [resultado] = await database.listar(expected.id);
+    deepStrictEqual(resultado, expected);
+  });
+
+  it("deve cadastrar um heroi, usando arquivos", async () => {
+    const expected =  DEFAULT_ITEM_CADASTRAR
+    const resultado = await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
+    const [actual] = await database.listar(DEFAULT_ITEM_CADASTRAR.id)
+    ok(actual, expected);
+
+  });
+});
